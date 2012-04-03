3da65b0359938ea2347cebf3fe8e396708de666f
Edd Grant <edd@eddgrant.com>
2012-04-03 14:21:33 +0100

    Updating ListParser closure to take List<String>

--- src/main/groovy/nineci/markdownplus/ListParser.groovy
+++ src/main/groovy/nineci/markdownplus/ListParser.groovy
@@ -3,13 +3,13 @@
  Authors: Joshua Burnett
  http://www.9ci.com/markdownplus
 
- Regex adapted from markdownj 
+ Regex adapted from markdownj
  Copyright (c) 2005, Martian Software
  Authors: Pete Bevin, John Mutchek
  http://www.martiansoftware.com/markdownj
- 
+
  Regex adapted from from PHP Markdown & Extra
- Copyright (c) 2004-2009 Michel Fortin  
+ Copyright (c) 2004-2009 Michel Fortin
  <http://michelf.com/projects/php-markdown/>
  */
 
@@ -19,11 +19,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class ListParser {
-	
+
 	MarkdownPlus markdownPlus
-	
+
 	int listLevel=0
-	
+
     MarkdownText parse(MarkdownText text) {
         int lessThanTab = MarkdownText.TAB_WIDTH - 1
 
@@ -50,15 +50,15 @@ class ListParser {
 			String list = m.group(1)
 			String listStart = m.group(3)
 			String listType = listStart.matches("[*+-]") ? 'ul':'ol'
-			
-			//turn 2 line feeds to 3 so that last items will be turned to a paragraph 
+
+			//turn 2 line feeds to 3 so that last items will be turned to a paragraph
 			list = list.replaceAll(/\n{2,}/, "\n\n\n");
 
 			String result = processListItems(list);
-		
+
 			def isUL
 			String linefeed = ""
-			
+
 			if(listLevel>0){
 				result = result.replaceAll(/\s+$/, "");
 				isUL = "ul" == listType
@@ -67,7 +67,7 @@ class ListParser {
 				linefeed ="\n"
 			}
 
-			return "<$listType>${linefeed}${result}</$listType>\n".toString()		
+			return "<$listType>${linefeed}${result}</$listType>\n".toString()
 		}
 		def pat = (listLevel>0) ? "^" : /(?:(?<=\n\n)|\A\n?)/
         Pattern matchStartOfLine = Pattern.compile(pat + wholeList, Pattern.MULTILINE);
@@ -77,10 +77,10 @@ class ListParser {
 
     String processListItems(String list) {
     	listLevel++;
-		
+
 		// Trim trailing blank lines:
 		list = list.replaceAll(/\n{2,}\z/, '\n');
-		
+
 		String listMarker = /[-+\*]|\d+[.]/
 		String pat = /(?m)(\n)?/ 		+
 			/^([ \t]*)/				+ //indentSpace
@@ -89,7 +89,7 @@ class ListParser {
 			/(\n{1,2}))/ 			+ //tailing blank lines
 			/(?=\n*(\z|\2(${listMarker})[ \t]+))/
 
-		list = list.replaceAll(pat){ String[] group->
+		list = list.replaceAll(pat){ List group-> //Groovy 1.8.6 now passes a List<String> rather than a String[]
 			String text = group[4];
 			MarkdownText item = new MarkdownText(text);
 			String indentSpace = group[1];
